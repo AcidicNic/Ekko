b53b7e0c658838600b6f6754e16fb4397fa1b06e
omarsagoo <om.sagoo@gmail.com>
2020-07-14 16:12:55 -0700

    change ip range

--- src/server/start.go
+++ src/server/start.go
@@ -21,7 +21,7 @@ func Start() {
 
 	// start the server on port :8080
 	log.Println("http server started on port :8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
